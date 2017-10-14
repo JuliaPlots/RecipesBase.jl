66548eb436c8e3c14505b83db771f25425feee17
Christian Kurz <c.kurz@tu-bs.de>
2017-10-14 12:34:23 +0200

    updated docstring `type` -> `struct`

--- src/RecipesBase.jl
+++ src/RecipesBase.jl
@@ -333,7 +333,7 @@ end
 """
 You can easily define your own plotting recipes with convenience methods:
 ```
-@userplot type GroupHist
+@userplot struct GroupHist
     args
 end
 @recipe function f(gh::GroupHist)
