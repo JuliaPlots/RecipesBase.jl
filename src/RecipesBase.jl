df5fba002faf886ddbfcc359dc24661cb0d05abb
t-bltg <tf.bltg@gmail.com>
2022-09-26 16:39:40 +0200

    document `@layout`

--- src/RecipesBase.jl
+++ src/RecipesBase.jl
@@ -569,6 +569,34 @@ function create_grid(s::Symbol)
     :((label = $(QuoteNode(s)), blank = $(s == :_)))
 end
 
+"""
+    @layout mat
+
+Generate the subplots layout from a matrix of symbols (where subplots can span multiple rows or columns).
+Precise sizing can be achieved with curly brackets, otherwise the free space is equally split between the plot areas of subplots.
+You can use the `_` character (underscore) to ignore plots in the layout (blank plots).
+
+# Examples
+
+```jldoctest
+julia> @layout([a b; c])
+2×1 Matrix{Any}:
+ Any[(label = :a, blank = false) (label = :b, blank = false)]
+ (label = :c, blank = false)
+
+julia> @layout [a{0.3w}; b{0.2h}]
+2×1 Matrix{Any}:
+ (label = :a, width = 0.3, height = :auto)
+ (label = :b, width = :auto, height = 0.2)
+
+julia> @layout([_ ° _; ° ° °; ° ° °])
+3×3 Matrix{Any}:
+ (label = :_, blank = true)   …  (label = :_, blank = true)
+ (label = :°, blank = false)     (label = :°, blank = false)
+ (label = :°, blank = false)     (label = :°, blank = false)
+
+```
+"""
 macro layout(mat::Expr)
     create_grid(mat)
 end
